211916a57f18e3b8746201c626c29d823411aee9
Ryan Johnston <RyanoJohnston@hotmail.com>
2021-03-20 19:58:16 +0000

    avg educations by year added, plot done

--- levelOfEducation.R
+++ levelOfEducation.R
@@ -7,15 +7,19 @@ names(levelOfEducation)[5] <- "Education_Level"
 names(levelOfEducation)[6] <- "Unit"
 names(levelOfEducation)[7] <- "Value"
 
-#removing duplicate column
-levelOfEducation = subset(levelOfEducation, select = -TLIST.Q1.) 
-
 #removing rows of year 2020 as they are unfinished
-levelOfEducation<-levelOfEducation[!(levelOfEducation$Quarter=="2020Q2"),]
+levelOfEducation <- levelOfEducation[!(levelOfEducation$Quarter=="2020Q2"),]
 
-      #casting numbers to each education
-
-
-
-
-
+    #male and female education dataframe creation - REMOVING BOTH SEXES ROWS
+    educationMaleAndFemale <- levelOfEducation[!(levelOfEducation$Sex=="Both sexes"),]
+    
+          #dataframe - average of each type of education
+             educationTypeAvg <- aggregate(educationMaleAndFemale$Value, by=list(educationMaleAndFemale$Education_Level,educationMaleAndFemale$Quarter), FUN = mean)
+                names(educationTypeAvg)[1] = "Education_Level"
+                names(educationTypeAvg)[2] = "Quarter"
+                names(educationTypeAvg)[3] = "Value"
+                
+            #PLOT SHOWING AVG EDUCATION & TYPE - BY YEAR
+          ggplot(educationTypeAvg, aes(x = Quarter, y = Value, color = Education_Level)) +
+            geom_point()+
+            theme_dark()
